a1d3851a8b9a429af08ac100b45ca849dd24b580
Hamid <hamidfzm@gmail.com>
2018-12-12 19:11:36 +0330

    Add root cmd

---
+++ cmd/dfs/dfs.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func newRootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "dfs",
+		Short: "Experimental Distributed File System",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Print("Hello World")
+		},
+		Version: "0.1.0",
+	}
+	return cmd
+}
+
+func main() {
+	cmd := newRootCommand()
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+}
